f475f145c7d3cb6d89d7b32ee641877d5cb1509c
Lebogang Prince Kula <lebogangkula24@gmail.com>
2026-01-19 13:06:15 +0200

    updated dist pages

---
+++ Angular20/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { View1 } from './view1/view1';
+import { View2 } from './view2/view2';
+
+  export const routes: Routes = 
+  [ { path: 'temp1',
+     component: View1 
+    }, 
+     { path: 'temp2',
+         component: View2 
+     },
+     { path: '**',
+     redirectTo: 'temp1',
+     pathMatch:"full" 
+     },
+     { path: '',
+     redirectTo: 'temp1',
+     pathMatch:"full" 
+     }
+ ];
